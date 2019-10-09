4309ed01143701730587ee4e3ef1f2074a3fd2dd
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-09 12:46:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 
 
 def find_the_cheese(array)
-cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camembert"]
    array.find do |list| 
      cheese_types.include?(list)
    end 
